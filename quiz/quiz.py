234776207779eb2f3842fb4abf6611f61a3c33c6
Marta Paz <pazmarta@gmail.com>
2019-02-11 12:43:47 +0000

    add menu and loop quiz game

---
+++ quiz/quiz.py
@@ -0,0 +1,24 @@
+
+def show_menu():
+    print("1. Ask questions")
+    print("2. Add a question")
+    print("3. Exit game")
+    
+    option = input("Enter option: ")
+    return option
+    
+def game_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            print ("You selected 'Ask a question option'")
+        elif option == "2":
+            print ("You selected 'Add a question option'")
+        elif option == "3":
+            break
+        else:
+            print("Invalid option")
+        print("")
+        
+game_loop()
+    
